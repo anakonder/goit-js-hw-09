769160c4572d9254c3ff41ad917428c1ec1fff2e
anakonder <120507138+anakonder@users.noreply.github.com>
2023-05-21 11:18:11 +0300

    timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+const delay = document.querySelector("delay");
+const step = document.querySelector("step");
+const amount = document.querySelector("amount");
+
+
+const promise = new Promise((resolve, reject) => {
+    timerId = setInterval(
+    function createPromise(position, delay) {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        
+      } else {
+        
+      }
+    }, delay)
+})
+
+
+
+
+
+
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
