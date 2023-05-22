7f254f74198cef1f58d88725ebb56557bf1a29a9
anakonder <120507138+anakonder@users.noreply.github.com>
2023-05-22 17:06:43 +0000

    Deploying to gh-pages from @ anakonder/goit-js-hw-09@d8b355ea72740f9b85865405782c5a99ea8bfae0 🚀

--- 03-promises.4236c372.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("delay");document.querySelector("step"),document.querySelector("amount"),new Promise((function(t,n){timerId=setInterval((function(e,t){Math.random()}),e)}))}();
-//# sourceMappingURL=03-promises.4236c372.js.map
